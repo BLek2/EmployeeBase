1ad37fe5550d852acb6ce0c321d5ad0ac379190a
Andrew Romanuk <blek213@gmail.com>
2017-09-23 20:50:09 +0300

    EF, create connection, models, contorller, views and etc...

---
+++ EmployeeBase/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeBase.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Read()
+        {
+            return View();
+        }
+        public ActionResult UpdateAndDelete()
+        {
+            return View();
+        }
+     
+    }
+}
---
+++ EmployeeBase/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace EmployeeBase.Models
+{
+    public class Employee
+    {
+     
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; } //The link to folder with photo
+        public string Biography { get; set; } //The link to folder with .doc 
+    }
+}
